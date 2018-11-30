38b466e9d0b9a7dc3a2d68c9ef78a371449c738a
Kanghee-Lee <jmkey96@naver.com>
2018-11-30 16:13:06 +0900

    Project(stage1) 완성본 (11/30 16:13)

---
+++ Project(stage1)/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		PhoneInfo frd1 = new PhoneInfo("¿Ã∞≠»Ò", "01012345678", "96/01/12");
+		PhoneInfo frd2 = new PhoneInfo("¿Ã∞ª»Ò", "01011111111");
+		
+		frd1.showInfo();
+		frd2.showInfo();
+	}
+
+}
---
+++ Project(stage1)/src/PhoneInfo.java
@@ -0,0 +1,22 @@
+
+public class PhoneInfo {
+	String name;
+	String phoneNumber;
+	String birthday;
+	
+	public PhoneInfo(String name, String phoneNumber, String birthday) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.birthday = birthday;
+	}
+	
+	public PhoneInfo(String name, String phoneNumber) {
+		this(name, phoneNumber, null);
+	}
+	
+	public void showInfo() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("전화번호 : " + this.phoneNumber);
+		System.out.println("생년월일 : " + this.birthday);
+	}
+}
