8f4862a50a9511c23fb960dd3bdb12a523e2182b
Kanghee-Lee <jmkey96@naver.com>
2018-12-04 15:31:07 +0900

    ch19-2(완성본) (12/04 15:30)

---
+++ ch19-2/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Business bs1 = new Business("±¸±Û", "computer");
+		PersonInfo per1 = new PersonInfo("kanghee", 23, bs1);
+		PersonInfo per2;
+		try {
+			per2 = (PersonInfo)per1.clone();
+			per1.showPersonInfo();
+			per2.showPersonInfo();
+		}
+		catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
